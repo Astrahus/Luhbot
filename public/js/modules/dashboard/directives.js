7e7a9a6a1b55352aa0c8027832598b951eb83550
Alisson Desandro Bovenzo <adbovenzo@gmail.com>
2015-07-23 00:29:03 -0300

    remove interfaces reset'

--- public/js/modules/dashboard/directives.js
+++ public/js/modules/dashboard/directives.js
@@ -10,9 +10,8 @@ angular.module('dashboard.directives',[])
             <i class="large material-icons">mode_edit</i>
           </a>
           <ul>
-            <li><a class="btn-floating red waves-effect waves-light tooltiped" data-position="left" data-delay="50" data-tooltip="Ligar" ng-click='ligar()'><i class="material-icons">power_settings_new</i></i></a></li>
-            <li><a class="btn-floating yellow darken-1 waves-effect waves-light tooltiped" data-position="left" data-delay="50" data-tooltip="Reiniciar" ng-click='reiniciar()'><i class="material-icons">replay</i></a></li>
-            <li><a class="btn-floating green waves-effect waves-light tooltiped" data-position="left" data-delay="50" data-tooltip="Ping" ng-click='ping()'><i class="material-icons">settings_remote</i></a></li>
+            <li><a class="btn-floating red waves-effect waves-light tooltipped" data-position="left" data-delay="50" data-tooltip="Ligar" ng-click='ligar()'><i class="material-icons">power_settings_new</i></i></a></li>
+            <li><a class="btn-floating green waves-effect waves-light tooltipped" data-position="left" data-delay="50" data-tooltip="Ping" ng-click='ping()'><i class="material-icons">settings_remote</i></a></li>
           </ul>
         </div>
       */
@@ -20,9 +19,6 @@ angular.module('dashboard.directives',[])
     link: function(scope,elem, attr){
       scope.ligar = function(){
         BOT.connect();
-      }
-      scope.reiniciar = function(){
-        BOT.restart()
       }
       scope.ping = function(){
         BOT.ping();
