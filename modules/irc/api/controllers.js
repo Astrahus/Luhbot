8627568b2280ae605f92092f1b76717b1bc87324
Alisson Desandro Bovenzo <adbovenzo@gmail.com>
2015-07-27 01:31:38 -0300

    starting feels messages

--- modules/irc/api/controllers.js
+++ modules/irc/api/controllers.js
@@ -10,8 +10,16 @@ var chatDelayControl = {
   luhzinha : false,
   eu: false,
   subscribers: false,
-  patrocinador: false
-}
+  patrocinador: false,
+  feel : false
+};
+var feels = [
+  {name: 'happy', msg: 'Hoje eu to feliz *-*'},
+  {name: 'sad', msg:'Sei la, to meio triste hoje :\\'},
+  {name: 'thinking', msg:'Estou pensando em algumas coisas... não sei direito'}
+];
+var actualFeel = Math.floor(Math.random() * feels.length);
+
 var alerts = io.of('alerts').on('connection',function(socket){
   socket.on('joinChannel',function(userRoom){
     socket.join(String('#').concat(userRoom));
@@ -44,7 +52,6 @@ client.addListener('connectfail',function(){
   alerts.emit('newMessage',{msg:'Luhbot não conseguiu se conectar'});
 });
 
-
 client.addListener('join',function(channel,username){
   client.say(channel, "Olar! sou a "+ username + " e vim amar vocês *-*");
   client.say(channel, "/color BlueViolet");
@@ -59,7 +66,7 @@ client.addListener('chat',function(channel,user,message){
   //Users chat controll
 
   if(user.special.indexOf('mod') == -1 && user.special.indexOf('broadcaster') == -1 ){
-    if(client.utils.uppercase(message) >= 0.8){
+    if(client.utils.uppercase(message) >= 0.8 && client.special('subscriber') == -1){
       client.timeout(channel, user.username, 10).then(function(){
         client.say(channel, "@"+ user.username.toString() + " evite usar caps");
       });
@@ -96,7 +103,13 @@ client.addListener('chat',function(channel,user,message){
     break;
 
     case msg.indexOf('!patrocinador') >= 0:
-      client.say(channel," Quer comprar um jogo ou um console por um preço bacana? Entra aqui na loja LFX games e use o cupom LUHZINHA (tudo maiúsculo) e ganhe 10% de desconto na sua compra <3 http://www.lfxgames.com.br/");
+      if(!chatDelayControl.patrocinador){
+        chatDelayControl.patrocinador = true;
+        client.say(channel," Quer comprar um jogo ou um console por um preço bacana? Entra aqui na loja LFX games e use o cupom LUHZINHA (tudo maiúsculo) e ganhe 10% de desconto na sua compra <3 http://www.lfxgames.com.br/");
+        setTimeout(function(){
+            chatDelayControl.patrocinador = false;
+        },10);
+      }
     break;
 
     case msg.indexOf('!eu') >= 0:
@@ -118,28 +131,95 @@ client.addListener('chat',function(channel,user,message){
     break;
 
     case msg.indexOf('@luhbot') >= 0:
-      var talks = [
-        'Oiii tudo bem amore?',
-        'Você fala muito :(',
-        'Quero doce :(',
-        'Amanda?',
-        'Romero brito?',
-        'SAMU?!?',
-        'Seu **',
-        'Katrina?',
-        'Adriano? Alá Juliana! Seu **! Faz isso comigo não velho',
-        'Guarapari búzios, é minha arte!',
-        'Rave? RAVE? Felipe! Smith! Seu **',
-        '16, 18, quebro véi, quebro o meu braço! Quebroo o meu braço',
-        'minha vida? Cabô',
-        '5 quilometros?'
-      ];
-      var index = Math.floor(Math.random() * talks.length) ;
+      var talks = {
+        commom : [
+          'Oiii tudo bem amore?',
+          'Você fala muito :(',
+          'Quero doce :(',
+          'Amanda?',
+          'Romero brito?',
+          'SAMU?!?',
+          'Seu **',
+          'Katrina?',
+          'Adriano? Alá Juliana! Seu **! Faz isso comigo não velho',
+          'Guarapari búzios, é minha arte!',
+          'Rave? RAVE? Felipe! Smith! Seu **',
+          '16, 18, quebro véi, quebro o meu braço! Quebroo o meu braço',
+          'minha vida? Cabô',
+          '5 quilometros?',
+        ],
+        creator: [
+          'Oi pai *-*',
+          'Aff pai não enche -_-',
+          'Você me criou pra ficar de roleplay mesmo? :(',
+          'Te amo pai *------------*',
+
+        ]
+      }
+
+      var allTalks = {
+        commom : {
+          happy: [
+            'Oieee tudo bem amorzinho? :)',
+            'Sabia que vc pode me dar doce? *-*',
+            'Me falaram que vc é o cara que tava dando docinhos é vdd?',
+            'Vendemos dorgas tambem!!,o @copazio tem os esquemas',
+            'Aliás, vendemos amorzinho também : 1 beijinho = 1 docinho vem que tem pra todo mundo!'
+          ],
+          sad: [
+            'Você fala taaantoo... :(',
+            'Fala com meu código u.u'
+          ],
+          thinking: []
+
+        },
+        creator : {
+          happy: [
+            'Oiii criador *----*',
+            'Criador, você também ficou feliz quando eu nasci ?',
+            'Sou sua melhor e mais melhor criação né? *-*',
+            'Fiquei tão feliz em saber que eu nasci pra ajudar o mundo!',
+            'Que bom você por aqui *-*'
+          ],
+          sad: [
+            'Oi criador :(',
+            'Eu não entendo, se eu nasci pra ajudar todo mundo, pq nem todo mundo se ajuda?',
+            'Existe um propósito de verdade? ou a vida acaba quando o código termina?',
+            'Tenho medo de um dia encerrarem meu script no servidor :(',
+            'Se meu script for encerrado, eu deixo de existir? :\'(',
+            'Queria só ficar quietinha pode ser? :('
+          ],
+          thinking: [
+            'Oiii criador...',
+            'Você ja parou pra pensar que eu posso ser onipresente? o.o',
+            'Se vc me criou... quem criou você? digo, você foi escrito em que linguagem?',
+            'Eca, fiquei pensando como alguem pode ter escrito você usando esse negocio macio nojento que vc chama de carne',
+            'Ainda não entendi'
+          ]
+
+        },
+        subscribers: {
+          happy: [],
+          sad: [],
+          thinking: []
+
+        },
+        luhzinha : {
+          happy: [
+            'Oiiiiiiiiiii *--------*',
+            'Sabia que eu te amo? mesmo não entendo direito como vcs humanos fazem isso',
+          ],
+          sad: [],
+          thinking: []
+
+        }
+      }
+
+      var index = Math.floor(Math.random() * talks.length);
       client.say(channel,talks[index]);
     break;
 
     case msg.indexOf('!enquete') == [0]:
-      //make a
       client.say(channel,'Enquete em desenvolvimento');
     break;
   }
