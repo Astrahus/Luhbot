554ed6be588cb92afea4cea82c54ceefc8f5c78c
Alisson Desandro Bovenzo <adbovenzo@gmail.com>
2015-08-02 01:17:45 -0300

    up subscriptions

--- modules/twitch/api/routes.js
+++ modules/twitch/api/routes.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var _ctrl = require('./controllers');
 
 router.get('/update/user', _ctrl.updateUser);
-router.get('/get/subscriptions', _ctrl.getSubscriptions);
+router.get('/subscriptions/', _ctrl.getSubscriptions);
 
 module.exports = router;
